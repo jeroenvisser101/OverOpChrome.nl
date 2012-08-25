8ac216790f3cb9c5ded91020640b90c0cb3f3de1
Jeroen Visser <jeroenvisser101@gmail.com>
2012-08-25 15:04:02 +0200

    Fixed paths, added translation

--- js/application.js
+++ js/application.js
@@ -1,6 +1,6 @@
 /*--- CODEKIT INCLUDES ----
 Mandatory for system:
-	@codekit-prepend "../libs/jquery/jquery.js"
+	@codekit-prepend "../libs/jquery/jquery.js", "../libs/i18next/i18n.js"
 
 Bootstrap prepends:
 	@codekit-prepend "../libs/bootstrap/js/bootstrap-transition.js", "../libs/bootstrap/js/bootstrap-tooltip.js", "../libs/bootstrap/js/bootstrap-affix.js", "../libs/bootstrap/js/bootstrap-alert.js", "../libs/bootstrap/js/bootstrap-button.js", "../libs/bootstrap/js/bootstrap-carousel.js", "../libs/bootstrap/js/bootstrap-collapse.js", "../libs/bootstrap/js/bootstrap-dropdown.js", "../libs/bootstrap/js/bootstrap-modal.js", "../libs/bootstrap/js/bootstrap-popover.js", "../libs/bootstrap/js/bootstrap-scrollspy.js", "../libs/bootstrap/js/bootstrap-tab.js", "../libs/bootstrap/js/bootstrap-typeahead.js"
@@ -8,3 +8,5 @@ Bootstrap prepends:
 Custom prepends:
 	@codekit-prepend "../js/custom-file.js"
 */
+
+i18n.init();
