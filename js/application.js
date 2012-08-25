8d2f4688baade3ad874abebae079edf981af4bba
Jeroen Visser <jeroenvisser101@gmail.com>
2012-08-25 19:07:00 +0200

    Version update: v0.2.1

--- js/application.js
+++ js/application.js
@@ -1,6 +1,6 @@
 /*--- CODEKIT INCLUDES ----
 Mandatory for system:
-	@codekit-prepend "../libs/jquery/jquery.js", "../libs/i18next/i18n.js"
+	@codekit-prepend "../libs/jquery/jquery.js", "../libs/i18next/i18n.js", "../libs/format/format.js"
 
 Bootstrap prepends:
 	@codekit-prepend "../libs/bootstrap/js/bootstrap-transition.js", "../libs/bootstrap/js/bootstrap-tooltip.js", "../libs/bootstrap/js/bootstrap-affix.js", "../libs/bootstrap/js/bootstrap-alert.js", "../libs/bootstrap/js/bootstrap-button.js", "../libs/bootstrap/js/bootstrap-carousel.js", "../libs/bootstrap/js/bootstrap-collapse.js", "../libs/bootstrap/js/bootstrap-dropdown.js", "../libs/bootstrap/js/bootstrap-modal.js", "../libs/bootstrap/js/bootstrap-popover.js", "../libs/bootstrap/js/bootstrap-scrollspy.js", "../libs/bootstrap/js/bootstrap-tab.js", "../libs/bootstrap/js/bootstrap-typeahead.js"
@@ -10,9 +10,28 @@ Custom prepends:
 */
 $.i18n.init({
 	lng: 'nl-NL',
-	useLocalStorage: false,
 	resGetPath: 'locales/__lng__/__ns__.json',
-	debug: true,
 }, function() {
 	$(document).i18n();
 });
+
+/* Disclaimer:
+This counter is based on the magic number "5.0735667174" wich is the amount of new users added every second */
+var updateTimer = function (){
+	var epoch = (new Date).getTime() / 1000;
+	var usersPerSecond = parseFloat(5.0735667174);
+	var users = 320000000;
+	var userDate = (new Date(2012, 4, 27)).getTime() / 1000;
+
+	// Calculate the number of seconds between now and the last update date for the usercount
+	var seconds = epoch - userDate;
+
+	// Calculate the numer of users:
+	var currentUsers = users + Math.floor(seconds * usersPerSecond);
+	$('.chrome-user-count').text(currentUsers.format('0.000').toString());
+
+	// Set new timeout
+	setTimeout(updateTimer, 1000);
+	return currentUsers;
+};
+updateTimer();
